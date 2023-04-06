8b2023d2c7b623592ac5ecaab567cd86f78011f7
andresflorez16 <andresflorez.dev@gmail.com>
2023-04-05 21:36:11 -0500

    fix project root

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,12 @@ import ArrowsIcon from './components/icons/ArrowsIcon'
 import SpeakerIcon from './components/icons/SpeakerIcon'
 import { Inter } from 'next/font/google'
 import LangSelector from './components/LangSelector'
-import useStore from '@/pages/hooks/useStore'
+import useStore from '@/utils/useStore'
 import { AUTO_LANGUAGE, VOICE_FOR_LANGUAGE } from '@/constants'
 import TextArea from './components/TextArea'
 import Layout from './components/Layout'
 import { useEffect } from 'react'
-import useDebounce from './hooks/useDebounce'
+import useDebounce from '@/pages/hooks/useDebounce'
 
 const inter = Inter({ weight: '400', subsets: ['latin'], display: 'swap', variable: '--font-inter' })
 
